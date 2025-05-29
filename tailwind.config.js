10e35af0b9f5b4065351fbacdb63d4b16db6bea1
Virginia Contreras <virginia.contreras@global66.com>
2025-05-29 17:24:55 -0500

    chore: update configuration and dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1D4ED8',
+        secondary: '#9333EA',
+        accent: '#F59E0B',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        title: ['"Bebas Neue"', 'sans-serif'],
+      },
+      spacing: {
+        72: '18rem',
+        84: '21rem',
+        96: '24rem',
+      },
+      borderRadius: {
+        xl: '1rem',
+      },
+      darkMode: 'class',
+    },
   },
   plugins: [],
 }
